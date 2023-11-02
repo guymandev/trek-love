2066a51f79c9529122ef0aded900b5c9827f8b70
Guy Jara <guy_jara@yahoo.com>
2023-11-01 23:43:08 -0700

    Got connected to API, added code to retrieve season data, and validated output in console.

--- trek-love/src/components/App/index.jsx
+++ trek-love/src/components/App/index.jsx
@@ -1,12 +1,41 @@
 import { useEffect, useState } from 'react'
 import { Routes, Route, Link } from "react-router-dom";
+// import HomePage from '../HomePage';
 import './styles.css'
 
 function App() {
-  
+  // Store API data here
+  const [episodes, setEpisodes] = useState([])
+  // const [detailsData, setDetailsData] = useState({})
+
+  // useEffect on episodes state variable so that I can 
+  // validate that it is getting populated.
+  useEffect(() => {
+    console.log(episodes);
+  }, [episodes]);
+
+  // Async function to query API & JSONify the response
+  async function getData(url) {
+    const res = await fetch(url)
+    const data = await res.json()
+    
+    let i = null
+    for (i = 1; i < 4; i++) {
+      let strSeason = `season/${i}`
+      setEpisodes(prevEpisodes => [...prevEpisodes, ...data[strSeason].episodes]);
+    }  
+  }
+
+  // Query the API for intial mount of the application
+  useEffect(() => {
+    const url = `https://api.themoviedb.org/3/tv/253?api_key=${import.meta.env.VITE_TMDB_API_KEY}&append_to_response=season/1,season/2,season/3`
+    getData(url)
+  }, [])
+
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <h1 className="text-3xl font-bold underline">Trek Love!</h1>
     </>
   )
 }
--- trek-love/src/main.jsx
+++ trek-love/src/main.jsx
@@ -5,7 +5,7 @@ import App from './components/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
 )
