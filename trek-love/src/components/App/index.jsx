756c57514b880756b6a437dd6a1b905d68603699
Guy Jara <guy_jara@yahoo.com>
2023-11-01 20:32:40 -0700

    Removed .svg files, installed Axios & Tailwind, updated configurations, and revalidated that the app is working.

---
+++ trek-love/src/components/App/index.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react'
+import { Routes, Route, Link } from "react-router-dom";
+import './styles.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+    </>
+  )
+}
+
+export default App
